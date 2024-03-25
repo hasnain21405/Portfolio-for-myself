5c4a0cb20f1e4c6b9ee8563388570c6b8081d6b8
Hasnain Sharif <138765252+hasnain21405@users.noreply.github.com>
2024-03-25 14:15:10 +0500

    finalized

--- java.js
+++ java.js
@@ -4,3 +4,12 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+function closePopup() {
+    document.getElementById('popup-container').style.display = 'none';
+}
+
+// Display the popup when the page loads
+window.onload = function() {
+    document.getElementById('popup-container').style.display = 'block';
+};
